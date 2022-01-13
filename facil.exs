915a186954e017dd05c4aad3b42035c8c7cf1233
roa-sh <juan.roa@kommit.co>
2022-01-12 21:26:13 -0500

    REadme update

--- facil.exs
+++ facil.exs
@@ -1,3 +1,91 @@
-Here
-Here
-Here
+# PECO - A platform to teach Computational thinking (CT) - Client
+
+PECO is a platform that allows a completely management in the learning paths 
+to the leave the students introduce in the world of the computer science while 
+they get fun doing didactic activities
+
+This repository only contains the Frontend (Client).
+
+Please make sure to visit the next repository to get the a completely functional 
+platform:
+[PECO - A platform to teach Computational thinking (CT) - Server](https://github.com/CristhianRodriguezMolina/plataforma-ct-backend)
+
+
+
+
+
+
+
+
+
+## Authors
+
+- [@CristhianRodriguezMolina](https://github.com/CristhianRodriguezMolina)
+- [@Wilzhar](https://github.com/Wilzhar)
+
+
+## Requirements
+
+- Node v14.15.1 or later
+- Npm v6.14.8 or later
+## Environment Variables
+
+To run this project, you will need to create **.env** file and add the following environment variables
+
+`REACT_APP_API_URL`: Set the server url to connect the client with the server
+
+If you are not completely sure to what values set, you can use the following default values:
+
+```
+REACT_APP_API_URL=http://localhost:4000
+```
+
+
+
+## Run the client server locally
+
+Clone the project
+
+- With https
+```bash
+  git clone https://github.com/CristhianRodriguezMolina/plataforma-ct-frontend.git
+```
+
+- With SSH
+```bash
+  git clone git@github.com:CristhianRodriguezMolina/plataforma-ct-frontend.git
+```
+
+Go to the project directory
+
+```bash
+  cd plataforma-ct-frontend
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm start
+```
+
+
+## Use the platfom
+
+Now you can use a web browser and navigate to `localhost:3000`
+
+Make sure to start the backend server too to connect the client:
+[PECO - A platform to teach Computational thinking (CT) - Server](https://github.com/CristhianRodriguezMolina/plataforma-ct-backend)
+
+The admin credentials to login are set in the server repository
+
+If you are using the default values for environment variables you can login using the next admin credentials:
+```
+User: 214896
+Password: Expedited-Wrongful3-Skewer
+```
